1182a9e8cccf11a398159ab7df287cbdd9be5417
Parralol <santinico36@hotmail.com>
2024-07-01 13:12:25 -0500

    infinite game

--- src/main/java/edu/escuelaing/arsw/ase/app/model/Monster.java
+++ src/main/java/edu/escuelaing/arsw/ase/app/model/Monster.java
@@ -1,5 +1,7 @@
 package edu.escuelaing.arsw.ase.app.model;
 
+import java.util.Random;
+
 public class Monster extends Actor {
     protected int vx;
     protected static final double FIRING_FREQUENCY = 0.01;
@@ -37,7 +39,27 @@ public class Monster extends Actor {
     public void collision(Actor a) {
         if (a instanceof Bullet || a instanceof Bomb) {
             remove();
+            spawn();
             stage.getPlayer().addScore(20);
+        }
+    }
+
+    private void spawn() {
+        Random rand = new Random();
+        int rand_int1 = rand.nextInt(10);
+        int rand_int2 = rand.nextInt(10);
+        if (rand_int1 == 5 || rand_int2 == 5) {
+            Crab m = new Crab(stage);
+            m.setX((int) (Math.random() * Stage.WIDTH));
+            m.setY(rand_int2 * 20);
+            m.setVx((int) (Math.random() * 20 - 10));
+            stage.addActor(m);
+        } else {
+            Monster m = new Monster(stage);
+            m.setX((int) (Math.random() * Stage.WIDTH));
+            m.setY(rand_int2 * 20);
+            m.setVx((int) (Math.random() * 20 - 10));
+            stage.addActor(m);
         }
     }
 }
